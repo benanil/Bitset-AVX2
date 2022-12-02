1bfa7b4afb9cce513dcc62765eeebce88bb338ac
Anılcan Gülkaya <48527900+benanil@users.noreply.github.com>
2022-12-03 01:44:04 +0300

    Update Bitset.hpp

--- Bitset.hpp
+++ Bitset.hpp
@@ -63,23 +63,17 @@ inline constexpr int PopCount(uint i)
 // from Faster Population Counts Using AVX2 Instructions resource paper
 FINLINE long VECTORCALL popcount256_epi64(__m256i v)
 {
-	const __m256i lookup = _mm256_setr_epi8(0, 1, 1, 2, 1, 2, 2, 3, 1, 2,
+	static const __m256i lookup = _mm256_setr_epi8(0, 1, 1, 2, 1, 2, 2, 3, 1, 2,
 			2, 3, 2, 3, 3, 4, 0, 1, 1, 2, 1, 2, 2, 3,
 			1, 2, 2, 3, 2, 3, 3, 4);
-	const __m256i low_mask = _mm256_set1_epi8(0x0f);
+	static const __m256i low_mask = _mm256_set1_epi8(0x0f);
 	__m256i lo =  _mm256_and_si256(v, low_mask);
 	__m256i hi = _mm256_and_si256(_mm256_srli_epi32(v, 4), low_mask);
 	__m256i popcnt1 = _mm256_shuffle_epi8(lookup, lo);
 	__m256i popcnt2 = _mm256_shuffle_epi8(lookup, hi);
-	__m256i total = _mm256_add_epi8(popcnt1, popcnt2);
-	
+	__m256i total = _mm256_add_epi8(popcnt1, popcnt2);	
 	v = _mm256_sad_epu8(total, _mm256_setzero_si256());
- 
-	long sum = _mm256_cvtsi256_si32(v);
-	sum += _mm256_cvtsi256_si32(_mm256_permute4x64_epi64(v, _MM_SHUFFLE(1, 1, 1, 1)));
-	sum += _mm256_cvtsi256_si32(_mm256_permute4x64_epi64(v, _MM_SHUFFLE(2, 2, 2, 2)));
-	sum += _mm256_cvtsi256_si32(_mm256_permute4x64_epi64(v, _MM_SHUFFLE(3, 3, 3, 3)));
-	return sum;
+	return _mm256_cvtsi256_si32(v) + _mm256_extract_epi64(v, 1) + _mm256_extract_epi64(v, 2) + _mm256_extract_epi64(v, 3);
 }
 template<int numBits> struct Bitset
 {
